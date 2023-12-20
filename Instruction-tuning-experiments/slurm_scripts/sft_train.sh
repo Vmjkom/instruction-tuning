7582e87c8382b48d24db2e8dc82b9d81e7c8b6fa
Ville Komulainen <vmjkom@utu.fi>
2023-12-20 14:15:34 +0200

    SftTrainer working with dolly

--- Instruction-tuning-experiments/slurm_scripts/sft_train.sh
+++ Instruction-tuning-experiments/slurm_scripts/sft_train.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=sft_train
+#SBATCH --job-name=trl_sft_poro_lora_dolly
 #SBATCH --account=project_462000241
-#SBATCH --partition=dev-g
+#SBATCH --partition=small-g
 #SBATCH --cpus-per-task=56
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --gpus-per-node=8
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=0
-#SBATCH -t 01:00:00
+#SBATCH --mem=480G
+#SBATCH --exclusive
+#SBATCH -t 02:00:00
 #SBATCH -o logs/%x-%j.out
 #SBATCH -e logs/%x-%j.err
 
 
-rm -f logs/debug_latest.out logs/debug_latest.err
-ln -s $SLURM_JOB_NAME-$SLURM_JOB_ID.out logs/debug_latest.out
-ln -s $SLURM_JOB_NAME-$SLURM_JOB_ID.err logs/debug_latest.err
+rm -f logs/latest.out logs/latest.err
+ln -s $SLURM_JOB_NAME-$SLURM_JOB_ID.out logs/latest.out
+ln -s $SLURM_JOB_NAME-$SLURM_JOB_ID.err logs/latest.err
 
 echo "JOB NAME" $SLURM_JOB_NAME
 echo "PARTITION" $SLURM_JOB_PARTITION
 echo "NNODES" $SLURM_NNODES
 echo "NCPUS" $SLURM_CPUS_PER_TASK
 echo "NODES" $SLURM_NODELIST
+echo "NGPUS" $SLURM_GPUS_ON_NODE
 
 module purge
 module use /appl/local/csc/modulefiles
@@ -28,34 +30,38 @@ module load pytorch
 source .venv/bin/activate
 
 export TORCH_EXTENSIONS_DIR=/tmp/$USER/torch_extensions
+rm -rf $TORCH_EXTENSIONS_DIR
 export CACHE=$TRANSFORMERS_CACHE
 export LOGS=/scratch/project_462000319/$USER/logs
 export PYTORCH_ROCM_ARCH=gfx90a
+export PYTHONPATH=.venv/lib/python3.10/site-packages/
 
 #Distributed variables
-export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
+#export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+#master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+#export MASTER_ADDR=$master_addr
 export LOCAL_RANK=$SLURM_LOCALID
-export RANK=$SLURM_PROCID
+#export RANK=$SLURM_PROCID
 #export WORLD_SIZE=$((SLURM_GPUS_ON_NODE*SLURM_NNODES))
 #export RDZV_HOST=$(hostname)
 #export RDZV_PORT=29400
 #echo master_addr
 
 
-#LOGGING
-export TORCH_DISTRIBUTED_DEBUG=DETAIL
-#export TRANSFORMERS_VERBOSITY=error
-#export TRANSFORMERS_NO_ADVISORY_WARNINGS=1
+#LOGGING/DEBUGGING
+export TRANSFORMERS_VERBOSITY=error
+export TRANSFORMERS_NO_ADVISORY_WARNINGS=1
 
 export OMP_NUM_THREADS=1
 
-srun -l python3 -m torch.distributed.run --standalone \
+srun --jobid $SLURM_JOBID bash -c "torchrun --role $(hostname -s): --tee 3 --nnodes  $SLURM_NNODES \
         --nproc-per-node=$SLURM_GPUS_ON_NODE \
+        --node_rank \$SLURM_PROCID \
         train_sft.py \
         --model LumiOpen/Poro-34B \
-        --training_data "oasst" \
-        --lang "en" \
         --num_train_epochs 1 \
-        --deepspeed_config ../ds-configs/stage2.json \
+        --save_directory /scratch/project_462000241/villekom/models/trl/sft \
+        --local_rank $LOCAL_RANK \
+        --deepspeed_config ./ds-configs/ds_config.json \
+        --use_peft \
+        --debug "
--- Instruction-tuning-experiments/train_sft.py
+++ Instruction-tuning-experiments/train_sft.py
@@ -2,22 +2,29 @@ import sys
 import os
 import torch
 import numpy as np
-from logging import warning
-from datasets import DatasetDict, Dataset
+import logging
+from datasets import DatasetDict, Dataset, load_dataset
 from argparse import ArgumentParser
 
 from transformers import (
     AutoTokenizer,
-    TrainingArguments
+    TrainingArguments,
+    AutoModelForCausalLM
 )
-
+from peft import (
+    get_peft_config,
+    get_peft_model,
+    get_peft_model_state_dict,
+    LoraConfig,
+    TaskType
+)
 from trl import (
     SFTTrainer,
     DataCollatorForCompletionOnlyLM
 )
 
 # custom classes
-from utils import load_model, logits_argmax
+from utils import load_model, logits_argmax, print_rank_0, is_last_rank
 from instruction_finetuning_datasets import read_data_sft
 
 user_token = "<|user|>"
@@ -29,61 +36,43 @@ def argparser():
     ap.add_argument('--model', type=str)
     ap.add_argument('--num_train_epochs', type=int, default=1)
     ap.add_argument('--per_device_batch_size', type=int, default=1)
-    ap.add_argument('--output_dir', type=str, default="output")
-    ap.add_argument('--output_file', type=str)
     ap.add_argument('--training_data', type=str, default="oasst")
     ap.add_argument('--lang', type=str, default="fi")
     ap.add_argument('--local_rank', type=int)
-    ap.add_argument('--use_peft', default=False, action='store_true')
+    ap.add_argument('--use_peft', action='store_true')
     ap.add_argument('--transformers_cache',type=str, default="/scratch/project_462000319/transformers_cache")
     ap.add_argument('--prompt_structure', default=False, type=lambda x: (str(x).lower() == 'true'))
     ap.add_argument('--save_directory',type=str)
+    ap.add_argument('--debug',action='store_true')
     return ap
 
-def filter_by_length(datasetdict, max_length):
-    for k in datasetdict:
-        dataset = datasetdict[k]
-        filtered = dataset.filter(lambda e: len(e['input_ids']) <= max_length)
-        orig_length = len(dataset['input_ids'])
-        filt_length = len(filtered['input_ids'])
-        if filt_length < orig_length:
-            warning(
-                f'filtered {k} from {orig_length} to {filt_length} '
-                f'({filt_length/orig_length:.1%}) by max_length {max_length}'
-            )
-            datasetdict[k] = filtered
-    return datasetdict
-
+#FOR DOLLY
 def formatting_prompts_func(example):
     output_texts = []
-    for i in range(len(example['prompt'])):
-        if not example['context'] or example['context'].isspace():
-            text = example['prompt']
-        else:
-            text = example['context'] + '\n' + example['prompt']
+    for i in range(len(example['INSTRUCTION'])):
+        text = f"{user_token}: {example['INSTRUCTION'][i]}\n {assistant_token}: {example['RESPONSE'][i]}"
         output_texts.append(text)
+        if i % 50 == 0:
+            logging.debug(f"Formatted prompt {text}")
     return output_texts
 
 
 def train_sft(args):
+   # if args.debug:
+   #     logging.basicConfig(level=logging.DEBUG)
     log_dir = './logs/'
-    base_model_name = os.path.basename(args.model)
-    output_dir = os.path.join(args.save_directory, base_model_name +
-                              "-" + args.training_data +
-                              "-" + args.lang +
-                              "-" + str(args.num_train_epochs) + "epochs")
-    print("Saving checkpoints to", output_dir)
-
+    base_model_name = os.path.join(args.model)
+    output_dir = os.path.join(args.save_directory)
     # This needs to be defined before model loading for deepspeed stage 3 to work correctly
     training_args = TrainingArguments(
         deepspeed=args.deepspeed_config,
         output_dir=output_dir,
         evaluation_strategy="steps",
         eval_steps=100,
-        learning_rate=args.learning_rate,
         per_device_train_batch_size=1,
         gradient_accumulation_steps=4,
         per_device_eval_batch_size=1,
+        eval_accumulation_steps=4,
         log_on_each_node=False,
         logging_strategy="steps",
         logging_steps=10,
@@ -93,50 +82,40 @@ def train_sft(args):
         save_total_limit=1,
         weight_decay=0.01,
         logging_dir=log_dir,
-        optim='adamw_hf',
         adam_epsilon=1e-8,
         max_grad_norm=1.0,
-        gradient_checkpointing=True,
+        gradient_checkpointing=False,
         report_to='tensorboard',
-        bf16=True,
         local_rank=args.local_rank,
+        bf16=True,
+        bf16_full_eval=True,
+        log_level='debug' if args.debug else 'info'
     )
-
     # TOKENIZER
-    print("===== Loading tokenizer =====")
-    print("tokenizer :", args.tokenizer)
+    print_rank_0("===== Loading tokenizer =====")
+    print_rank_0(f"tokenizer : {args.model}")
     tokenizer = AutoTokenizer.from_pretrained(args.model)
-    print("===== Loaded tokenizer =====")
+    tokenizer.padding_side = 'right'
+    print_rank_0("===== Loaded tokenizer =====")
 
     # MODEL
-    print("===== Loading model =====")
-    print("base model:", args.model)
-    model = load_model(args.model, args.transformers_cache, args.use_lora)
-    print("===== Loaded model =====")
+    print_rank_0("===== Loading model =====")
+    print_rank_0(f"base model:, {args.model}")    
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model,
+        torch_dtype=torch.bfloat16,
+        cache_dir=args.transformers_cache,
+        )
+    print_rank_0("===== Loaded model =====")
+    if args.use_peft:
+        peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=6, lora_alpha=32,
+                                     lora_dropout=0.1)
+    else:
+        peft_config = None
+    print_rank_0("Loaded peft config")
 
-    # # add special tokens if necessary
-    # if tokenizer.pad_token is None:
-    #     tokenizer.add_special_tokens({'pad_token': '<|pad|>'})
-    # if tokenizer.sep_token is None:
-    #     tokenizer.add_special_tokens({'sep_token': '<|endofprompt|>'})
-    # model.resize_token_embeddings(len(tokenizer))
-
-    train_data = read_data_sft(args.training_data, split="train", lang=args.lang)
-    val_data = read_data_sft(args.training_data, split="valid", lang=args.lang)
-    eval_data = read_data_sft(args.training_data, split="eval", lang=args.lang)
-
-    print("Size of training data", len(train_data))
-    print("Size of validation data", len(val_data))
-    print("Size of evaluation data", len(eval_data))
-
-    dataset = DatasetDict({
-        'train': train_data,
-        'validation': val_data,
-        'evaluation': eval_data,
-    })
-
-    print("Size of training data", len(dataset['train']))
-
+    dataset = load_dataset("Villekom/oa_dolly_15k_fi",split="train").train_test_split(test_size=0.1)
+    print_rank_0("Loaded dataset")
     instruction_template = "<|user|>"
     response_template = "<|assistant|>"
     collator = DataCollatorForCompletionOnlyLM(instruction_template=instruction_template,
@@ -147,31 +126,29 @@ def train_sft(args):
         model=model,
         args=training_args,
         train_dataset=dataset['train'],
-        eval_dataset=dataset['validation'],
+        eval_dataset=dataset['test'],
         data_collator=collator,
         tokenizer=tokenizer,
         formatting_func=formatting_prompts_func,
-        max_length=4028,
-        gradient_checkpointing=True,
-        gradient_accumulation_steps=2
-        #preprocess_logits_for_metrics=logits_argmax,
+        max_seq_length=2048,
+        peft_config=peft_config
     )
-
+    print_rank_0("Starting training")
     trainer.train()
     base_model_name = os.path.basename(args.model)
-    save_directory = os.path.join("../models/sft_finetuned/", base_model_name + "-" + args.training_data + "-" + args.lang)
-    if args.use_lora:
-        trainer.model.save_pretrained(save_directory + "-lora")
+    save_directory = os.path.join(args.save_directory, base_model_name)
+    if args.use_peft:
+        trainer.model.save_pretrained(save_directory + "-peft")
     else:
         trainer.save_model(save_directory)
-    eval_results = trainer.evaluate(dataset['evaluation'])
+    #eval_results = trainer.evaluate(dataset['evaluation'])
 
-    print('Training data', args.training_data)
-    print('Model:', args.model)
-    print('Learning rate:', args.learning_rate)
-    print('batch size:', args.per_device_batch_size)
-    print('Gradient accumulation steps:', args.gradient_accumulation_steps)
-    print('Evaluation results:', eval_results['eval_loss'])
+    #print_rank_0('Training data', args.training_data)
+    print_rank_0(f'Model:, {args.model}')
+    print_rank_0(f'Learning rate:, {args.learning_rate}')
+    print_rank_0(f'batch size:, {args.per_device_batch_size}')
+    print_rank_0(f'Gradient accumulation steps:, {args.gradient_accumulation_steps}')
+    print_rank_0(f"Evaluation results:, {eval_results['eval_loss']}")
 
 def main(argv):
     args = argparser().parse_args(argv[1:])
